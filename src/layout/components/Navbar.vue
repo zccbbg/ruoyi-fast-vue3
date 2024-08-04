8c677a64f41eaa2f97d73fa787c2403c4c9a6e98
zccbbg <zccbbg@qq.com>
2024-08-04 16:35:59 +0800

    style: layout 调整，默认主题改为light，去掉一些阴影特效 navvar tags view 背景色调整

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -105,7 +105,7 @@ function setLayout() {
   height: 50px;
   overflow: hidden;
   position: relative;
-  background: #fff;
+  background: #F8F9FD;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
 
   .hamburger-container {
